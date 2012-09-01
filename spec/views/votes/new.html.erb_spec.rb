d8da29de047b4bea9cda606e65dcf40f0b2aa1d5
Robin Dunlop <rdunlop@thoughtworks.com>
2012-09-01 17:50:06 -0500

    fix remaining vote-view test

--- spec/views/votes/new.html.erb_spec.rb
+++ spec/views/votes/new.html.erb_spec.rb
@@ -2,21 +2,16 @@ require 'spec_helper'
 
 describe "votes/new" do
   before(:each) do
-    assign(:vote, stub_model(Vote,
-      :proposal_id => 1,
-      :user_id => 1,
-      :vote => "MyString"
-    ).as_new_record)
+    vote = assign(:vote, FactoryGirl.create(:vote))
+    @proposal = vote.proposal
   end
 
   it "renders new vote form" do
     render
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => votes_path, :method => "post" do
-      assert_select "input#vote_proposal_id", :name => "vote[proposal_id]"
-      assert_select "input#vote_user_id", :name => "vote[user_id]"
-      assert_select "input#vote_vote", :name => "vote[vote]"
+    assert_select "form", :action => proposal_votes_path(@proposal), :method => "post" do
+      assert_select "select#vote_vote", :name => "vote[vote]"
     end
   end
 end
