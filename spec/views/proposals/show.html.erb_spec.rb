033e60f5d0fb65bf943b6096e57cb22f9112cbb4
Robin Dunlop <rdunlop@thoughtworks.com>
2012-09-02 15:25:13 -0500

    fix some broken view tests

--- spec/views/proposals/show.html.erb_spec.rb
+++ spec/views/proposals/show.html.erb_spec.rb
@@ -2,24 +2,15 @@ require 'spec_helper'
 
 describe "proposals/show" do
   before(:each) do
-    @proposal = assign(:proposal, stub_model(Proposal,
-      :committee_id => 1,
-      :status => "Status",
-      :transition_straight_to_vote => false,
-      :owner_id => 2,
-      :summary => "MyText",
-      :title => "MyText"
-    ))
+    @proposal = assign(:proposal, FactoryGirl.create(:proposal, :title => "thetitle"))
+    @revision = FactoryGirl.create(:revision, :proposal => @proposal, :background => "thebackground")
+    @comment = @proposal.comments.new
   end
 
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/1/)
-    rendered.should match(/Status/)
-    rendered.should match(/false/)
-    rendered.should match(/2/)
-    rendered.should match(/MyText/)
-    rendered.should match(/MyText/)
+    rendered.should match(/thetitle/)
+    rendered.should match(/thebackground/)
   end
 end
