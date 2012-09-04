6127d136cd326a90a8b62e4fd19b79b91d652bda
Robin Dunlop <rdunlop@thoughtworks.com>
2012-09-03 19:41:17 -0500

    fix remaining test by adding ability mock

--- spec/views/revisions/show.html.erb_spec.rb
+++ spec/views/revisions/show.html.erb_spec.rb
@@ -2,8 +2,15 @@ require 'spec_helper'
 
 describe "revisions/show" do
   before(:each) do
-    @revision = assign(:revision, FactoryGirl.create(:revision))
+    @revision = FactoryGirl.create(:revision)
     @proposal = @revision.proposal
+    @comment = @proposal.comments.new
+
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    controller.stub(:current_ability) { @ability }
+    # XXX can? nothing: @ability.can :something, @proposal
+
   end
 
   it "renders attributes in <p>" do
@@ -12,6 +19,5 @@ describe "revisions/show" do
     rendered.should match(/MyText1/)
     rendered.should match(/MyText2/)
     rendered.should match(/MyText3/)
-    rendered.should match(/MyText4/)
   end
 end
