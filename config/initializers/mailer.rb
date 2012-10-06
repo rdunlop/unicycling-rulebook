99ebd27c1bb42a67451b3917a2fab1b133161c78
Robin Dunlop <rdunlop@thoughtworks.com>
2012-10-06 13:51:19 -0500

    allow disabling ssl for smtp e-mailer, update README to detail the settings for iuf mailer

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -5,7 +5,7 @@ ActionMailer::Base.smtp_settings = {
     :user_name            => ENV['MAIL_USERNAME'],
     :password             => ENV['MAIL_PASSWORD'],
     :authentication       => "plain",  
-    :enable_starttls_auto => true  
+    :enable_starttls_auto => (ENV['MAIL_TLS'].nil? ? true : ENV['MAIL_TLS'] == 'true')
 }  
 ActionMailer::Base.default :from => ENV['MAIL_FULL_EMAIL']
 
