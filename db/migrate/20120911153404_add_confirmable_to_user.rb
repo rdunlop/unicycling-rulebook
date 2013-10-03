723ee264d1c7cac30c9a994d66a7d10312fb4e35
Robin Dunlop <rdunlop@thoughtworks.com>
2013-10-03 04:56:03 +0000

    Update rulebook to use the new vagrant system, and update devise migration, and documentation. Also add a license file.

--- db/migrate/20120911153404_add_confirmable_to_user.rb
+++ db/migrate/20120911153404_add_confirmable_to_user.rb
@@ -1,7 +1,9 @@
 class AddConfirmableToUser < ActiveRecord::Migration
   def change
     change_table(:users) do |t|
-        t.confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
     end
   end
 end
