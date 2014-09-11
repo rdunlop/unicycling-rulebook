42e0009880ad5d465350d793c1e7a0c5ede6005c
Robin Dunlop <robin@dunlopweb.com>
2014-09-10 22:56:55 -0500

    fix unpermitted parameters when creating new user object

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   check_authorization :unless => :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:name, :location, :comments]
+  end
+
+  public
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
