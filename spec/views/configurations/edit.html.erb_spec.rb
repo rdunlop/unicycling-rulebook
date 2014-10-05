5c5f1efc1710107019d53c76a05906e560a9a0ba
Robin Dunlop <robin@dunlopweb.com>
2014-10-05 11:17:59 -0500

    remove remnant of splitting rulebook and configuration

--- spec/views/configurations/edit.html.erb_spec.rb
+++ spec/views/configurations/edit.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "rulebooks/edit", :type => :view do
+describe "configurations/edit", :type => :view do
   before(:each) do
     @rulebook = assign(:rulebook, FactoryGirl.build_stubbed(:rulebook,
       :rulebook_name => "MyString",
@@ -14,7 +14,7 @@ describe "rulebooks/edit", :type => :view do
     render
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => rulebooks_path(@rulebook), :method => "post" do
+    assert_select "form", :action => configuration_path(@rulebook), :method => "post" do
       assert_select "input#rulebook_rulebook_name", :name => "rulebook[rulebook_name]"
       assert_select "textarea#rulebook_front_page", :name => "rulebook[front_page]"
       assert_select "textarea#rulebook_faq", :name => "rulebook[faq]"
