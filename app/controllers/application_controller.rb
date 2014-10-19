071478f9138cca99a91bd9b9415b78658cb350db
Robin Dunlop <robin@dunlopweb.com>
2014-10-19 15:45:08 -0500

    include google analytics on all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   check_authorization :unless => :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  layout "rulebook"
 
   protected
 
