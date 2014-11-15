1d71ffe1996ce8b941b5270a079f09fb9b251cab
Robin Dunlop <robin@dunlopweb.com>
2014-11-15 01:21:47 -0600

    add stage cap deploy

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,5 +1,5 @@
-set :deploy_to, '/home/ec2-user/unicycling-rulebook'
-set :rails_env, 'production'
+set :deploy_to, '/home/ec2-user/unicycling-rulebooktest'
+set :rails_env, 'stage'
 set :branch, 'master'
 
 server '54.69.105.140', user: 'ec2-user', roles: %w(web app db)
