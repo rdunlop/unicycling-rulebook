09c4cbd616768706c518730ec57890a26e71ceb4
Robin Dunlop <robin@dunlopweb.com>
2015-08-15 09:53:06 -0500

    update stage server ip address to new instance

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -2,4 +2,4 @@ set :deploy_to, '/home/ec2-user/unicycling-rulebooktest'
 set :rails_env, 'stage'
 set :branch, 'master'
 
-server '54.148.148.88', user: 'ec2-user', roles: %w(web app db)
+server '52.25.119.104', user: 'ec2-user', roles: %w(web app db)
