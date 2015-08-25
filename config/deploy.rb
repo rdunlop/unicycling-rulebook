386fa88d7f35317e1594602267dd3b3bf46c2af0
Robin Dunlop <robin@dunlopweb.com>
2015-08-24 20:47:44 -0500

    upgrade capistrano gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
+# config valid only for Capistrano 3.4
+lock '3.4.0'
 
 set :application, 'unicycling-rulebook'
 set :repo_url, 'git@github.com:rdunlop/unicycling-rulebook.git'
