1d518cf669b482ab3c999111c0946d71683e53b0
Robin Dunlop <robin@dunlopweb.com>
2015-08-27 08:53:38 -0500

    Begin Moving to Pundit authorization policy

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '4.2.4'
 gem 'devise'
 gem "devise-async"
 gem 'cancancan', '~> 1.9'
+gem 'pundit'
 
 # front end
 gem 'haml'
---
+++ app/policies/application_policy.rb
@@ -0,0 +1,69 @@
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+
+    #raise Pundit::NotAuthorizedError, "must be logged in" unless @user
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    scope.where(id: record.id).exists?
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+  private
+
+  def admin?
+    user.admin?
+  end
+
+  def committee_admin?(committee = nil)
+    user.is_committee_admin(committee)
+  end
+
+  def committee_editor?(committee = nil)
+    user.is_committee_editor(committee)
+  end
+
+  def scope
+    Pundit.policy_scope!(user, record.class)
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope
+    end
+  end
+end
