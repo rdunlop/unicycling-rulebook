b36fe4c198cde319a6a40a5e40d3f54ed4cd0ca3
Robin Dunlop <robin@dunlopweb.com>
2015-12-06 18:19:56 -0600

    clean up, remove debug code

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,8 +2,8 @@
 class ConfirmationsController < Devise::ConfirmationsController
   # Remove the first skip_before_filter (:require_no_authentication) if you
   # don't want to enable logged users to access the confirmation page.
-  skip_before_filter :require_no_authentication
-  skip_before_filter :authenticate_user!
+  skip_before_action :require_no_authentication
+  skip_before_action :authenticate_user!
 
   # PUT /resource/confirmation
   def update
@@ -14,17 +14,16 @@ class ConfirmationsController < Devise::ConfirmationsController
           do_confirm
         else
           do_show
-          @confirmable.errors.clear #so that we wont render :new
+          @confirmable.errors.clear # so that we wont render :new
         end
       else
         @confirmable.errors.add(:email, :password_already_set)
       end
-      binding.pry
     end
 
-    if !@confirmable.errors.empty?
+    unless @confirmable.errors.empty?
       self.resource = @confirmable
-      render 'devise/confirmations/new' #Change this if you don't have the views on default path
+      render 'devise/confirmations/new' # Change this if you don't have the views on default path
     end
   end
 
@@ -39,7 +38,7 @@ class ConfirmationsController < Devise::ConfirmationsController
     end
     unless @confirmable.errors.empty?
       self.resource = @confirmable
-      render 'devise/confirmations/new' #Change this if you don't have the views on default path
+      render 'devise/confirmations/new' # Change this if you don't have the views on default path
     end
   end
 
@@ -47,16 +46,14 @@ class ConfirmationsController < Devise::ConfirmationsController
 
   def with_unconfirmed_confirmable
     @confirmable = User.find_or_initialize_with_error_by(:confirmation_token, params[:confirmation_token])
-    if !@confirmable.new_record?
-      @confirmable.only_if_unconfirmed {yield}
-    end
+    @confirmable.only_if_unconfirmed {yield} unless @confirmable.new_record?
   end
 
   def do_show
     @confirmation_token = params[:confirmation_token]
     @requires_password = true
     self.resource = @confirmable
-    render 'devise/confirmations/show' #Change this if you don't have the views on default path
+    render 'devise/confirmations/show' # Change this if you don't have the views on default path
   end
 
   def do_confirm
