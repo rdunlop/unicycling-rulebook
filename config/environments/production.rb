97d11e25eea0fab9756b571a3d6ac40ba59606fa
Robin Dunlop <robin@dunlopweb.com>
2016-09-18 10:51:06 -0500

    set the production log level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ RulebookApp::Application.configure do
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
