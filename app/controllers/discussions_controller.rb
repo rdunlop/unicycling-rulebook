01165c082ddacee598fd0a87e73e6aaefe68591c
Robin Dunlop <robin@dunlopweb.com>
2017-02-12 12:56:24 -0600

    allow not-signed-in to view discussions

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -14,7 +14,7 @@
 #
 
 class DiscussionsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
   before_action :load_committee, only: [:new, :create]
   before_action :set_committee_breadcrumb, only: [:new, :create]
   before_action :load_discussion, only: [:show, :close]
--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,5 +1,5 @@
 class CommentPolicy < ApplicationPolicy
   def create?
-    (user.admin || user.is_in_committee(record.discussion.committee)) && record.discussion.is_open_for_comments?
+    user && (user.admin || user.is_in_committee(record.discussion.committee)) && record.discussion.is_open_for_comments?
   end
 end
