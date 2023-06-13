6d4d8db6192a16424fb5e223b729dc01fe3d8d37
Robin Dunlop <robin@dunlopweb.com>
2023-06-13 16:07:56 -0500

    Change from master to main

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,6 +1,6 @@
 set :deploy_to, '/home/ec2-user/unicycling-rulebook'
 set :rails_env, 'production'
-set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
+set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "main"
 
 # new prod
 server '54.148.66.79', user: 'ec2-user', roles: %w[web app db]
--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,6 +1,6 @@
 set :deploy_to, '/home/ec2-user/unicycling-rulebook'
 set :rails_env, 'stage'
-set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
+set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "main"
 
 server '52.25.119.104', user: 'ec2-user', roles: %w[web app db]
 
