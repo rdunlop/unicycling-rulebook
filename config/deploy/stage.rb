dbe5485bae41948130b833839c1d216d8d5cf61c
Robin Dunlop <robin@dunlopweb.com>
2025-03-13 13:59:20 -0500

    update server Ips

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -2,6 +2,7 @@ set :deploy_to, '/home/ec2-user/unicycling-rulebook'
 set :rails_env, 'stage'
 set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "main"
 
-server '52.25.119.104', user: 'ec2-user', roles: %w[web app db]
+# server '52.25.119.104', user: 'ec2-user', roles: %w[web app db]
+server '35.164.69.42', user: 'ec2-user', roles: %w[web app db]
 
 set :no_deploytags, true
