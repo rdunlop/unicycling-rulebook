b969619f74785ec68ca1df699486c2e83083ad13
Robin Dunlop <robin@dunlopweb.com>
2026-06-09 20:23:15 -0500

    add code to build and push docker image through CI

--- config/puma.rb
+++ config/puma.rb
@@ -3,9 +3,9 @@ threads threads_count, threads_count
 
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-# Bind to the same UNIX socket that Nginx is already configured to proxy to.
-# Keeping this path avoids any Nginx reconfiguration.
-if ENV["RAILS_ENV"] == "development"
+# In development or when PORT is set (e.g. ECS/Fargate), bind to TCP.
+# Otherwise bind to the Unix socket that Nginx proxies to on EC2.
+if ENV["RAILS_ENV"] == "development" || ENV["PORT"]
   port ENV.fetch("PORT") { 3000 }
 else
   bind "unix:///tmp/unicorn-unicycling-rulebook.socket"
@@ -18,7 +18,8 @@ preload_app!
 pidfile "tmp/pids/puma.pid"
 state_path "tmp/pids/puma.state"
 
-stdout_redirect "log/puma.stdout.log", "log/puma.stderr.log", true
+# Redirect to files on EC2; let stdout flow to CloudWatch when PORT is set (ECS).
+stdout_redirect "log/puma.stdout.log", "log/puma.stderr.log", true unless ENV["PORT"]
 
 before_worker_boot do
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
---
+++ terraform/global/backend.hcl
@@ -0,0 +1,5 @@
+bucket       = "unicycling-rulebook-terraform-state"
+key          = "global/terraform.tfstate"
+region       = "us-west-2"
+use_lockfile = true
+encrypt      = true
